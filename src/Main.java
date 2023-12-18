4b802120fb7d0f48a4c5fb5b5095dc7d8f5923d4
ShreeRuba <rubashree0361@gmail.com>
2023-12-18 16:28:31 +0530

    Added Files

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,39 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Create employees and sales reps
+        Employee employee = new Employee("Rubashree", "Ashokkumar", 1, 22, 2500, 13, 50000, 3, "Shree_Ruba", "shree1234");
+        SalesRep salesRep = new SalesRep("Vishnu", "Prakash", 2, 29, 3000, 15, 80000, 9, "Vishnu_Murali", "prakash1234", 100000);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Create sales manager
+        SalesManager salesManager = new SalesManager("Randy", "Orton", 3, 35, 1500, 10, 90000, 10, "Viper", "viper1234");
+
+        // Add sales rep to sales manager's team
+        salesManager.addSalesRep(salesRep);
+
+        // Test login functionality
+        System.out.println("Employee login: " + employee.login("Shree_Ruba", "shree1234"));
+        System.out.println("Employee Bonus: $" + employee.calculateBonus());
+        System.out.println("Time To Retirement :" + employee.timeToRetirement());
+        System.out.println("Vacation time Left :" + employee.vacationTimeLeft() +"\n");
+
+
+        System.out.println("SalesRep login: " + salesRep.login("Vishnu_Murali", "prakash1234"));
+        System.out.println("SalesRep Bonus: $" + salesRep.calculateBonus());
+        System.out.println("Time To Retirement :" + salesRep.timeToRetirement());
+        System.out.println("Commision :" + salesRep.calculateCommission());
+        System.out.println("Vacation time Left :" + salesRep.vacationTimeLeft() +"\n");
+
+        System.out.println("SalesManager login: " + salesManager.login("Viper", "viper1234"));
+        System.out.println("SalesManager Bonus: $" + salesManager.calculateBonus());
+        System.out.println("Time To Retirement :" + salesManager.timeToRetirement());
+        System.out.println("Commision :" + salesManager.calculateCommission());
+        System.out.println("Vacation time Left :" +salesManager.vacationTimeLeft() +"\n");
+
+
     }
 }
